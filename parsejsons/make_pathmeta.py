20282c8422bed5137bb5cf5d52733114a59e4c2e
tedunderwood <tunder@illinois.edu>
2018-05-15 22:42:16 -0500

    code for parsing jsons

---
+++ parsejsons/make_pathmeta.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# make_pathmeta
+
+import pandas as pd
+import csv
+
+meta = pd.read_csv('../metadata/newgenremeta.csv')
+
+ids2get = set(meta.docid.tolist())
+
+rows2keep = []
+
+with open('../../noveltmmeta/get_EF/ids2pathlist.tsv', encoding = 'utf-8') as f:
+    reader = csv.DictReader(f, delimiter = '\t')
+    fieldnames = reader.fieldnames
+    for row in reader:
+        docid = row['docid']
+        if docid not in ids2get:
+            continue
+        else:
+            rows2keep.append(row)
+
+with open('ids2pathlist4genres.tsv', mode = 'w', encoding = 'utf-8') as f:
+    writer = csv.DictWriter(f, fieldnames = fieldnames, delimiter = '\t')
+    writer.writeheader()
+    for row in rows2keep:
+        writer.writerow(row)
+
+
+
